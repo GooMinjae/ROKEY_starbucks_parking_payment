ccf2bf420067390d6e2eb707e246ce0e418b2b47
ming <999aldwo@gmail.com>
2025-04-02 00:04:57 +0900

    ui 수정 1차

--- gmj/barcode_gui/sejin_thread/barcode_gui.py
+++ gmj/barcode_gui/sejin_thread/barcode_gui.py
@@ -14,31 +14,53 @@ class BarcodeScannerApp(QWidget):
     def initUI(self):
         self.setWindowTitle('바코드 스캐너')
         self.setGeometry(100, 100, 640, 480)
+        self.setStyleSheet("background-color: #252E3E;")
         
         self.video_label = QLabel(self, text="카메라 화면")
         self.video_label.setFixedSize(640, 480)
         self.video_label.setAlignment(Qt.AlignCenter)
+        self.video_label.setStyleSheet('color: #FFFFFF;\
+                                        border-radius: 5;\
+                                        border-color: #00704A;\
+                                        border-style : solid;\
+                                        border-width : 3px')
 
         self.scan_button = QPushButton("스캔 시작")
         self.scan_button.clicked.connect(self.start_scanning)
+        self.scan_button.setFixedHeight(50)
+        self.scan_button.setStyleSheet("""
+                                    QPushButton {
+                                        background-color: #134F9E;
+                                        color: #FFFFFF;
+                                        border-radius: 5;
+                                    }
+                                    QPushButton:hover {
+                                        background-color: #0F3F80;
+                                    }
+                                    QPushButton:pressed {
+                                        background-color: #0B2B57;
+                                    }
+                                """)
         
         # self.barcode_info = QTextEdit(self)
         # self.barcode_info.setReadOnly(True)
         self.barcode_info = QLabel("")
         self.barcode_info.setAlignment(Qt.AlignCenter)
         self.barcode_info.setFixedHeight(80)
+        self.barcode_info.setStyleSheet("color: #FFFFFF;")
         
         layout = QVBoxLayout()
         layout.addWidget(self.video_label)
         layout.addWidget(self.barcode_info)
         layout.addWidget(self.scan_button)
         self.setLayout(layout)
-    
+
+    # 버튼 연결안할 시 init으로
     def start_scanning(self):
         self.scanner_worker = BarcodeScannerWorker()
         self.scanner_worker.frameCaptured.connect(self.update_frame)
         self.scanner_worker.barcodeDetected.connect(self.display_barcode_info)
-        self.scanner_worker.start()  # 앱 실행 시 자동으로 스캐너 시작
+        self.scanner_worker.start()
 
 
     def update_frame(self, frame):
@@ -53,7 +75,7 @@ class BarcodeScannerApp(QWidget):
         free_amount = barcode_data.split('-')[1]
 
         self.barcode_info.setText(f'날짜: {obj_nowdate.date()}\n시간: {obj_nowdate.time()}\n가격: {free_amount}')
-        # print(f'날짜: {info["date"]}\n시간: {info["time"]}\n가격: {info["price"]}')   # 테스트용 출력
+        print(f'날짜: {obj_nowdate.date()}\n시간: {obj_nowdate.time()}\n가격: {free_amount}')   # 테스트용 출력
         self.close_app()
     
     def close_app(self):
@@ -70,4 +92,7 @@ if __name__ == '__main__':
     window.show()
     # sys.exit(app.exec_())
     app.exec_()
-    window.scanner_worker.stop()
+    try:
+        window.scanner_worker.stop()
+    except:
+        pass
